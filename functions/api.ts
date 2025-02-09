81ca80dc18591e1fc9aa3bfa136de1efbca16750
Boghey <dan.boghean@gmail.com>
2025-02-09 21:24:02 +0000

    Update api routes

--- functions/api.ts
+++ functions/api.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import serverless from "serverless-http";
+import { registerRoutes } from "../server/routes";
 
 const app = express();
 
-registerRoutes(app)
+registerRoutes(app);
 export const handler = serverless(app);
