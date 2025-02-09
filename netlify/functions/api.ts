51cc4685f1686bcb03568bbffc733628f75cb151
Boghey <dan.boghean@gmail.com>
2025-02-09 17:08:12 +0000

    Test new functions

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,11 +1,10 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
 
-const api = express();
-
-const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
-
-api.use("/api/", router);
-
-export const handler = serverless(api);
+export async function handler(event, context) {
+  const app = express();
+  const router = Router();
+  router.get("/", (req, res) => res.send("Hello World!"));
+  app.use("/api/", router);
+  return serverless(app)(event, context);
+}
